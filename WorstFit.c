fa129ebd3e0aae48c4a571ea86d2bfb3e021b6a5
SaiyamChopra <saiyamchopra447@gmail.com>
2025-04-23 00:53:23 +0530

    Updated WorstFit.c

--- WorstFit.c
+++ WorstFit.c
@@ -1,28 +1,48 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdbool.h>
-void main() { 
-    int n, m, max; 
-    bool first = false; 
-    printf("Enter the number of partitions: "); 
-    scanf("%d", &n); 
-    int arr[n];  
-    printf("\nEnter the elements: "); 
-    for (int i = 0; i < n; i++) { 
-        scanf("%d", &arr[i]); 
-    }  
-    printf("Enter the memory required for process: "); 
-    scanf("%d", &m); 
-    for (int i = 0; i < n; i++) { 
-        if (arr[i] >= m) { 
-            if (!first || arr[i] > max) { 
-                max = arr[i]; 
-                first = true; 
-            } 
-        } 
-    } 
-    if (first) { 
-        printf("Memory allocated is %d\n", max); 
-    } else { 
-        printf("No suitable memory block available.\n"); 
-    } 
+
+int main() {
+    int nb, np;
+
+    // Input number of memory partitions
+    printf("Enter the number of memory partitions: ");
+    scanf("%d", &nb);
+    int partitions[nb], partitionAllocated[nb];
+
+    printf("Enter the sizes of memory partitions:\n");
+    for (int i = 0; i < nb; i++) {
+        scanf("%d", &partitions[i]);
+        partitionAllocated[i] = 0; // 0 = not allocated
+    }
+
+    // Input number of processes
+    printf("Enter the number of processes: ");
+    scanf("%d", &np);
+    int processes[np];
+
+    printf("Enter the memory required for each process:\n");
+    for (int i = 0; i < np; i++) {
+        scanf("%d", &processes[i]);
+    }
+
+    // Worst Fit Allocation
+    for (int i = 0; i < np; i++) {
+        int worstIdx = -1;
+        for (int j = 0; j < nb; j++) {
+            if (!partitionAllocated[j] && partitions[j] >= processes[i]) {
+                if (worstIdx == -1 || partitions[j] > partitions[worstIdx]) {
+                    worstIdx = j;
+                }
+            }
+        }
+
+        if (worstIdx != -1) {
+            printf("Process %d allocated to partition of size %d\n", i + 1, partitions[worstIdx]);
+            partitionAllocated[worstIdx] = 1;
+        } else {
+            printf("Process %d cannot be allocated memory.\n", i + 1);
+        }
+    }
+
+    return 0;
 }
