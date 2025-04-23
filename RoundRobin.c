4bfd2a027eecb1f7599eaa4ac256d4a1393cd243
SaiyamChopra <saiyamchopra447@gmail.com>
2025-04-23 11:44:00 +0530

    Updated RoundRobin.c

--- RoundRobin.c
+++ RoundRobin.c
@@ -1,85 +1,84 @@
-/*Enter the number of processes: 3
-Enter Arrival Time and Burst Time for P1: 0 5
-Enter Arrival Time and Burst Time for P2: 1 3
-Enter Arrival Time and Burst Time for P3: 2 1
-Enter Time Quantum: 2
-
-Process	Turnaround Time	Waiting Time
-P3	    3		        2
-P2	    7		        4
-P1	    9		        4
-
-Average Waiting Time: 3.33
-Average Turnaround Time: 6.33
-*/
 #include <stdio.h>
-#define MAX 100
+#include <string.h>
 
 int main() {
-    int bt[MAX], rt[MAX], at[MAX], wt[MAX], tat[MAX];
-    int i, n, time = 0, remain, tq;
-    float avg_wt = 0, avg_tat = 0;
-    int sequence[MAX * 10]; // Stores execution order
-    int seq_index = 0;
-
+    int n, quantum;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-    remain = n;
 
-    for (i = 0; i < n; i++) {
-        printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
-        scanf("%d%d", &at[i], &bt[i]);
-        rt[i] = bt[i];
+    int burst[n], remaining[n], waiting[n], turnaround[n], completion[n];
+    char processes[n][10];
+    for (int i = 0; i < n; i++) {
+        printf("Enter process name: ");
+        scanf("%s", processes[i]);
+        printf("Enter burst time for %s: ", processes[i]);
+        scanf("%d", &burst[i]);
+        remaining[i] = burst[i];
+        waiting[i] = 0;
+        turnaround[i] = 0;
     }
 
-    printf("Enter Time Quantum: ");
-    scanf("%d", &tq);
+    printf("Enter the time quantum: ");
+    scanf("%d", &quantum);
 
-    printf("\nExecution Sequence:\n");
+    int time = 0, completed = 0;
+    char ganttProcesses[100][10];
+    int ganttTimes[100];
+    int ganttIndex = 0;
 
-    int done[MAX] = {0};
+    while (completed < n) {
+        for (int i = 0; i < n; i++) {
+            if (remaining[i] > 0) {
+                ganttTimes[ganttIndex] = time;
+                strcpy(ganttProcesses[ganttIndex], processes[i]);
+                ganttIndex++;
 
-    while (remain != 0) {
-        int executed = 0;
-        for (i = 0; i < n; i++) {
-            if (at[i] <= time && rt[i] > 0) {
-                executed = 1;
-
-                // Record process execution
-                sequence[seq_index++] = i + 1;
-                if (rt[i] <= tq) {
-                    time += rt[i];
-                    rt[i] = 0;
-                    tat[i] = time - at[i];
-                    wt[i] = tat[i] - bt[i];
-                    avg_wt += wt[i];
-                    avg_tat += tat[i];
-                    remain--;
-                    done[i] = 1;
+                if (remaining[i] <= quantum) {
+                    time += remaining[i];
+                    remaining[i] = 0;
+                    completed++;
+                    completion[i] = time;
                 } else {
-                    rt[i] -= tq;
-                    time += tq;
+                    time += quantum;
+                    remaining[i] -= quantum;
                 }
             }
         }
-
-        if (!executed)
-            time++;
     }
 
-    // Print execution order
-    for (i = 0; i < seq_index; i++) {
-        if (i != 0) printf(" -> ");
-        printf("P%d", sequence[i]);
+    ganttTimes[ganttIndex] = time;
+
+    float totalWaiting = 0, totalTurnaround = 0;
+    for (int i = 0; i < n; i++) {
+        turnaround[i] = completion[i];
+        waiting[i] = turnaround[i] - burst[i];
+        totalWaiting += waiting[i];
+        totalTurnaround += turnaround[i];
     }
 
-    printf("\n\nProcess\tTurnaround Time\tWaiting Time\n");
-    for (i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\n", i + 1, tat[i], wt[i]);
+    float avgWaiting = totalWaiting / n;
+    float avgTurnaround = totalTurnaround / n;
+
+    printf("\n");
+    printf("| %-16s | %-10s | %-14s | %-14s |\n", "Process", "Burst Time", "Waiting Time", "Turnaround Time");
+    printf("--------------------------------------------------------------------------\n");
+    for (int i = 0; i < n; i++) {
+        printf("| %-16s | %-10d | %-14d | %-14d |\n", processes[i], burst[i], waiting[i], turnaround[i]);
     }
 
-    printf("\nAverage Waiting Time: %.2f", avg_wt / n);
-    printf("\nAverage Turnaround Time: %.2f\n", avg_tat / n);
+    printf("\nAverage Waiting Time: %.2f\n", avgWaiting);
+    printf("Average Turnaround Time: %.2f\n", avgTurnaround);
 
+    printf("\nGantt Chart:\n");
+    for (int i = 0; i < ganttIndex; i++) {
+        printf("| %-3s ", ganttProcesses[i]);
+    }
+    printf("|\n");
+
+    for (int i = 0; i <= ganttIndex; i++) {
+        printf("%-5d", ganttTimes[i]);
+    }
+
+    printf("\n");
     return 0;
 }
