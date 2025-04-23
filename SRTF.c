0fbac01bed4b1ef4228d8c2e22bbc59d2c3804f3
SaiyamChopra <saiyamchopra447@gmail.com>
2025-04-23 10:43:37 +0530

    Updated SRTF.c

--- SRTF.c
+++ SRTF.c
@@ -1,28 +1,14 @@
-/*Enter number of processes: 3
-Enter arrival time and burst time for each process:
-P1 arrival time: 0
-P1 burst time: 7
-P2 arrival time: 2
-P2 burst time: 4
-P3 arrival time: 4
-P3 burst time: 1
-
-Process	Arrival	Burst	Completion	Turnaround	Waiting
-P3	    4	    1	    5		    1		    0
-P2	    2	    4	    9		    7		    3
-P1	    0	    7	    16		    16		    9
-
-Average Waiting Time = 4.00  
-Average Turnaround Time = 8.00
-*/
 #include <stdio.h>
 #define MAX 20
 
 int main() {
-    int bt[MAX], at[MAX], rt[MAX]; // Burst, Arrival, Remaining times
-    int i, smallest, count = 0, time, n;
-    int wait_time = 0, turnaround_time = 0, end;
+    int bt[MAX], at[MAX], rt[MAX], ct[MAX]; // ct = completion time
+    int i, smallest, count = 0, time, n, end;
+    int wait_time = 0, turnaround_time = 0;
     float avg_wait_time, avg_turnaround_time;
+
+    int exec_seq[MAX * 100]; // Store execution sequence
+    int seq_index = 0;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
@@ -36,9 +22,7 @@ int main() {
         rt[i] = bt[i];
     }
 
-    rt[19] = 9999; // Sentinel value
-
-    printf("\nProcess\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n");
+    rt[19] = 9999; // Sentinel
 
     for (time = 0; count != n; time++) {
         smallest = 19;
@@ -48,18 +32,33 @@ int main() {
             }
         }
 
+        exec_seq[seq_index++] = smallest + 1; // Log execution
+
         rt[smallest]--;
 
         if (rt[smallest] == 0) {
             count++;
             end = time + 1;
-            int tat = end - at[smallest];     // Turnaround time
-            int wt = tat - bt[smallest];      // Waiting time
-            wait_time += wt;
-            turnaround_time += tat;
-
-            printf("P%d\t%d\t%d\t%d\t\t%d\t\t%d\n", smallest + 1, at[smallest], bt[smallest], end, tat, wt);
+            ct[smallest] = end;
         }
+    }
+
+    printf("\nExecution Timeline:\n0 ");
+    for (i = 1; i < seq_index; i++) {
+        if (exec_seq[i] != exec_seq[i - 1]) {
+            printf("-> P%d -> %d ", exec_seq[i - 1], i);
+        }
+    }
+    printf("-> P%d -> %d\n", exec_seq[seq_index - 1], seq_index);
+
+    // Calculate and show results
+    printf("\nProcess\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n");
+    for (i = 0; i < n; i++) {
+        int tat = ct[i] - at[i];
+        int wt = tat - bt[i];
+        turnaround_time += tat;
+        wait_time += wt;
+        printf("P%d\t%d\t%d\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], ct[i], tat, wt);
     }
 
     avg_wait_time = (float)wait_time / n;
